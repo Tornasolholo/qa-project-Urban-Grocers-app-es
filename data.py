843d6fe86401d183bad605a993e000fb05de3929
AngelaMV <159729840+Tornasolholo@users.noreply.github.com>
2024-08-06 22:35:00 -0500

    Update data.py

---
+++ data.py
@@ -0,0 +1,16 @@
+#paso1: Almacenamiento de los encabezados de la creación de usuario (PESTANA CREACION DE CTA)
+headers = {
+    "Content-Type": "application/json"
+}
+
+#paso 2: Almacenamiento de la estructura del Cuerpo de la creación de usuario, (CONJUNTO MINIMO DE DATOS)
+user_body = {
+    "firstName": "Max",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+}
+
+# paso 3: Almacenamiento de la estructura del Cuerpo del kit (PESTANA DE CREACION DE CTA)
+kit_body = {
+    "name": "Mi conjunto"
+}
