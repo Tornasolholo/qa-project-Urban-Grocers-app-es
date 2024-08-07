614ad32a32ec445fac1b2ec65d27e8f508076cd3
AngelaMV <159729840+Tornasolholo@users.noreply.github.com>
2024-08-06 22:34:13 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,5 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE = "https://cnt-2c490e75-84b1-45ef-9fe7-44d0b5573338.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+
+CREATE_USER_PATH = "/api/v1/users/" # Almacena la ruta para crear un usuario o usuaria en esta variable (PESTANA CREACION DE CTA)
+KITS_PATH = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable (PESTANA MAIN PRODUCT)
