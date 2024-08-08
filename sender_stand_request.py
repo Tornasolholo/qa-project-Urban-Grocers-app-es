4601013d2d2bb259733b536081aa9bb744f4d256
AngelaMV <159729840+AngelaDMV@users.noreply.github.com>
2024-08-08 09:10:30 -0500

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,11 +2,6 @@ import configuration
 import requests
 import data
 
-def get_docs():
-    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
-'''response = get_docs()
-print(response.status_code)'''
-
 
 def post_new_user(body):
     return requests.post(configuration.URL_SERVICE+configuration.CREATE_USER_PATH,
